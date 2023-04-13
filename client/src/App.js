9b74eb110aa3dca918de9dc0f374414252fbad4b
Anas jaidi <anas.jaidi@icloud.com>
2023-04-13 12:13:34 +0000

    make the first request properly

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css'
 import Chat from './component/chat';
 import { Switch, Route } from 'react-router-dom';
-
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import {useEffect} from "react";
+import axios from "axios";
 
 function App() {
+    useEffect(async () => {
+        // const data = await fetch('/api/v1/user/')
+
+        const data = await axios.get('/api/v1/user/')
+
+        console.log(data)
+    }, [])
+
   return (
     <div className="w-screen h-screen bg-black/90">
         <Layout>
