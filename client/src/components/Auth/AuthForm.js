42758aa5746168b6dd30ac55b375fc3f818722f2
Anas jaidi <anas.jaidi@icloud.com>
2023-04-13 12:21:21 +0000

    finish sign up

--- client/src/components/Auth/AuthForm.js
+++ client/src/components/Auth/AuthForm.js
@@ -1,13 +1,31 @@
-import { useState } from 'react';
+import { useState , useRef} from 'react';
 
 import classes from './AuthForm.module.css';
+import axios from "axios";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
 
+  const passwordRef = useRef()
+  const emailRef = useRef();
+
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
+
+  const submitFormHandler = async e => {
+    e.preventDefault()
+    const entredEmailValue = emailRef.current.value
+    const entredPasswordValue = passwordRef.current.value
+
+    const data = await axios.post('/api/v1/user/signup', {
+      firstName: "new name",
+      lastName: "new last name",
+      email: entredEmailValue,
+      password: entredPasswordValue
+    })
+    console.log(data)
+  }
 
   return (
     <section className={classes.auth}>
@@ -15,14 +33,14 @@ const AuthForm = () => {
       <form>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordRef}/>
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button onClick={submitFormHandler}>{isLogin ? 'Login' : 'Create Account'}</button>
           <button
             type='button'
             className={classes.toggle}
