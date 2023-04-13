61edb6c4c0ba710b917afd80c94fff395e9ffd6d
FRAGG33R <aissambarchil75@gmail.com>
2023-04-13 18:07:18 +0000

    style navbar logIn signIn component

--- client/src/components/Auth/AuthForm.js
+++ client/src/components/Auth/AuthForm.js
@@ -1,98 +1,99 @@
-import {useState, useRef} from 'react';
+import { useState, useRef } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 import axios from "axios";
 
 const AuthForm = () => {
-    const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
 
-    const passwordRef = useRef()
-    const emailRef = useRef();
-    const firstNameRef = useRef();
-    const lastNameRef = useRef();
+  const passwordRef = useRef();
+  const emailRef = useRef();
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
 
-    const switchAuthModeHandler = () => {
-        setIsLogin((prevState) => !prevState);
-    };
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
 
-    const submitFormHandler = async e => {
-        e.preventDefault()
-        const entredEmailValue = emailRef.current.value
-        const entredPasswordValue = passwordRef.current.value
+  const submitFormHandler = async (e) => {
+    e.preventDefault();
+    const entredEmailValue = emailRef.current.value;
+    const entredPasswordValue = passwordRef.current.value;
 
-        let endpoint;
-        let req;
+    let endpoint;
+    let req;
 
-        if (isLogin) {
-            endpoint = '/api/v1/user/signin'
+    if (isLogin) {
+      endpoint = "/api/v1/user/signin";
 
-            req = {
-                email: entredEmailValue,
-                password: entredPasswordValue
-            }
-        } else {
+      req = {
+        email: entredEmailValue,
+        password: entredPasswordValue,
+      };
+    } else {
+      const entredFirstNameValue = firstNameRef.current.value;
+      const entredLastNameValue = lastNameRef.current.value;
+      endpoint = "/api/v1/user/signup";
 
-            const entredFirstNameValue = firstNameRef.current.value
-            const entredLastNameValue = lastNameRef.current.value
-            endpoint = '/api/v1/user/signup'
-
-            req = {
-                firstName: entredFirstNameValue,
-                lastName: entredLastNameValue,
-                email: entredEmailValue,
-                password: entredPasswordValue
-            }
-
-        }
-
-
-        try {
-
-            const data = await axios.post(endpoint, req)
-        } catch (e) {
-            alert(e.response.data.message)
-        }
-
-        // console.log(data)
+      req = {
+        firstName: entredFirstNameValue,
+        lastName: entredLastNameValue,
+        email: entredEmailValue,
+        password: entredPasswordValue,
+      };
     }
 
-    return (
-        <section className={classes.auth}>
-            <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-            <form>
-                {
-                    !isLogin && <>
-                        <div className={classes.control}>
-                            <label htmlFor='firstName'>first name</label>
-                            <input type='text' id='firstName' required ref={firstNameRef}/>
-                        </div>
-                        <div className={classes.control}>
-                            <label htmlFor='lastName'>last name</label>
-                            <input type='text' id='lastName' required ref={lastNameRef}/>
-                        </div>
-                    </>
-                }
-                <div className={classes.control}>
-                    <label htmlFor='email'>Your Email</label>
-                    <input type='email' id='email' required ref={emailRef}/>
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor='password'>Your Password</label>
-                    <input type='password' id='password' required ref={passwordRef}/>
-                </div>
-                <div className={classes.actions}>
-                    <button onClick={submitFormHandler}>{isLogin ? 'Login' : 'Create Account'}</button>
-                    <button
-                        type='button'
-                        className={classes.toggle}
-                        onClick={switchAuthModeHandler}
-                    >
-                        {isLogin ? 'Create new account' : 'Login with existing account'}
-                    </button>
-                </div>
-            </form>
-        </section>
-    );
+    try {
+      const data = await axios
+        .post(endpoint, req)
+        .then(() => {
+			
+		})
+        .catch(() => {});
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form>
+        {!isLogin && (
+          <>
+            <div className={classes.control}>
+              <label htmlFor="firstName">first name</label>
+              <input type="text" id="firstName" required ref={firstNameRef} />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="lastName">last name</label>
+              <input type="text" id="lastName" required ref={lastNameRef} />
+            </div>
+          </>
+        )}
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passwordRef} />
+        </div>
+        <div className={classes.actions}>
+          <button onClick={submitFormHandler}>
+            {isLogin ? "Login" : "Create Account"}
+          </button>
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default AuthForm;
